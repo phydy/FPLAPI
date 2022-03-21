ee77da7f5523da2bf6acf813466f9737cf27344e
phydy <mphidel@gmail.com>
2022-03-21 11:17:54 +0300

    static

--- FPLAPI/settings.py
+++ FPLAPI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-3+h5y41q&8veb^@-(85grppt&iv^q_1-k=(=4y1hg@---_ho#u"#os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
